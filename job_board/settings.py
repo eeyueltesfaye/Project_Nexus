4d7671b5bb3d6cee0c2382a605b0d0d3a60b9912
eeyueltesfaye <eyueltesfaye6@gmail.com>
2025-08-04 01:16:19 +0300

    feat: integrate Swagger for API documentation

--- job_board/settings.py
+++ job_board/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- job_board/urls.py
+++ job_board/urls.py
@@ -17,12 +17,32 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include 
+from django.urls import path, include, re_path
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Job Board API",
+      default_version='v1',
+      description="API documentation for the Job Board platform",
+      contact=openapi.Contact(email="eyueltesfaye6@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/', include('jobs.urls')),
+     # Swagger and Redoc endpoints
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] 
 
 # To serve media files during development
