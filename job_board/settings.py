dc539645acc91d6b6fd58aa95607c7d367e9cf79
eeyueltesfaye <eyueltesfaye6@gmail.com>
2025-08-07 02:23:39 +0300

    final testing in development env

--- job_board/settings.py
+++ job_board/settings.py
@@ -34,7 +34,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
+SECRET_KEY = "2=p_j%ey!q-i@490-^9%v*-gdqsarc_jb@5d=+nj8sz_sgw4sy"
+
 
 
 
